f2ba1a7ad989aa4d70381653c8cc01c78e1b9877
test-saitomini <tennis.lp.710@gmail.com>
2022-08-05 22:22:53 +0900

    20220805 portfolio

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 $pdo = new PDO("mysql:dbname=heroku_7f44de0a892964f;host=us-cdbr-east-06.cleardb.net;charset=utf8","b51f5ef5ea5d11","5edf58c2");
 
-phpinfo();
+echo "データ接続成功";
+//phpinfo();
 
 //header('Location: ./top.php');
 //exit();
