a40f2757e73ace92124f79d3b39f8664e7c7034a
test-saitomini <tennis.lp.710@gmail.com>
2022-08-06 21:34:01 +0900

    portfolio 20220806

--- schedule_update.php
+++ schedule_update.php
@@ -13,7 +13,6 @@ if($_SESSION != NULL){
 
 $id = $_POST['id'];
             
-mb_internal_encoding("UTF-8");
 $pdo = new PDO("mysql:dbname=heroku_7f44de0a892964f;host=us-cdbr-east-06.cleardb.net;charset=utf8","b51f5ef5ea5d11","5edf58c2");
             
 $stmt = $pdo -> query('select * from schedule where id = '.$id);
